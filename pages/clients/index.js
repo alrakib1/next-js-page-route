c97479d94234bf3b945c2755cd27facffc160f7d
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-04 04:14:50 +0600

    different ways of link components

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -7,22 +7,29 @@ const ClientPage = () => {
     { id: "rafi", name: "Rafi" },
     { id: "shakib", name: "Shakib" },
     { id: "mushfiq", name: "Mushfiq" },
-    { id: "tamim", name: "Tamim"},
-    { id: "mahmudullah", name: "Mahmudullah"},
-    { id: "mashrafe", name: "Mashrafe"},
-    { id: "mehedi", name: "Mehedi"},
+    { id: "tamim", name: "Tamim" },
+    { id: "mahmudullah", name: "Mahmudullah" },
+    { id: "mashrafe", name: "Mashrafe" },
+    { id: "mehedi", name: "Mehedi" },
   ];
 
   return (
     <div>
-      <h2 className="mt-5 text-center font-bold text-5xl"> This is the client page</h2>
+      <h2 className="mt-5 text-center font-bold text-5xl">
+        {" "}
+        This is the client page
+      </h2>
       <ul className="flex gap-4 ml-10 mt-5">
         {clients.map((client) => (
           <li
             className="text-white hover:bg-yellow-600 px-2 py-1 rounded-md transition-colors duration-150 bg-slate-400"
             key={client.id}
           >
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            <Link
+              href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
