25dabe282a369bc888d67801a96769a1aee935fc
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-04 04:11:26 +0600

    added dynamic details page for clients

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,25 +1,39 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 const ClientDetails = () => {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query);
 
-    const router = useRouter();
-    console.log(router.pathname);
-    console.log(router.query);
+  const projects = [
+    { id: "ProjectA", name: "Project A" },
+    { id: "ProjectB", name: "Project B" },
+    { id: "ProjectC", name: "Project C" },
+    { id: "ProjectD", name: "Project D" },
+  ];
 
-    return (
-        <div>
-           <h2 className='text-4xl text-center font-semibold'> this is the details page of client {router.query.id}</h2>
-           <ul>
-                <li>
-                        <a href={`/clients/${router.query.id}/ProjectA`}>Project A</a>
-                </li>
-                <li>
-                     <a href={`/clients/${router.query.id}/ProjectB`}>Project B</a>
-                </li>
-           </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-4xl text-center font-semibold">
+        {" "}
+        this is the details page of client {router.query.id}
+      </h2>
+      <ul className="mt-5 flex gap-2 justify-center">
+        {projects.map((project) => (
+          <li
+            className="text-white hover:bg-yellow-600 px-2 py-1 rounded-md transition-colors duration-150 bg-slate-400"
+            key={project.id}
+          >
+            <Link href={`/clients/${router.query.id}/${project.name}`}>
+              {project.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ClientDetails;
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -2,17 +2,30 @@ import Link from "next/link";
 import React from "react";
 
 const ClientPage = () => {
+  const clients = [
+    { id: "rakib", name: "Rakib" },
+    { id: "rafi", name: "Rafi" },
+    { id: "shakib", name: "Shakib" },
+    { id: "mushfiq", name: "Mushfiq" },
+    { id: "tamim", name: "Tamim"},
+    { id: "mahmudullah", name: "Mahmudullah"},
+    { id: "mashrafe", name: "Mashrafe"},
+    { id: "mehedi", name: "Mehedi"},
+  ];
+
   return (
     <div>
-      <ul className="flex gap-4 ml-10">
-        <li>
-          <Link href="/clients/rakib">Rakib</Link>
-        </li>
-        <li>
-          <Link href="/clients/rafi">Rafi</Link>
-        </li>
+      <h2 className="mt-5 text-center font-bold text-5xl"> This is the client page</h2>
+      <ul className="flex gap-4 ml-10 mt-5">
+        {clients.map((client) => (
+          <li
+            className="text-white hover:bg-yellow-600 px-2 py-1 rounded-md transition-colors duration-150 bg-slate-400"
+            key={client.id}
+          >
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
       </ul>
-      <h2 className="mt-5"> This is the client page</h2>
     </div>
   );
 };
